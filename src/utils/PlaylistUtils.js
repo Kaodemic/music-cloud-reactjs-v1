90a53b61a4f4172c5339a9dde185091d459d49d2
Tran Khanh <tran_khanh@outlook.com>
2021-05-21 19:15:12 +0700

    util playlist

--- src/utils/PlaylistUtils.js
+++ src/utils/PlaylistUtils.js
@@ -1,4 +1,10 @@
-import { SESSION_LIKES_URL, SESSION_STREAM_URL, SONGS_URL } from './../constants/ApiConstants';
+import moment from 'moment';
+import { denormalize } from 'normalizr';
+import {
+    SESSION_LIKES_URL,
+    SESSION_STREAM_URL,
+    SONGS_URL
+} from './../constants/ApiConstants';
 import {
     GENRE_PLAYLIST_TYPE,
     GENRE_QUERY_MAP,
@@ -7,10 +13,56 @@ import {
     SESSION_LIKES_PLAYLIST,
     SESSION_STREAM_PLAYLIST
 } from './../constants/PlaylistConstants';
+import { songSchema } from './../constants/Schemas';
 
 const isFetching = (playlist, playlists) => (playlist in playlists ?
     playlists[playlist].isFetching :
     false
+);
+
+const genrePlaylistUrl = (genre, time) => {
+    const genreUriSegment = `&tags=${GENRE_QUERY_MAP[genre] || genre}`;
+    const timeUriSegment = time ? `&created_at[from]=${moment().subtract(Number(time), 'days').format('YYYY-MM-DD%2012:00:00')}` : '';
+
+    return `${SONGS_URL}${timeUriSegment}${genreUriSegment}`;
+};
+
+const searchPlaylistUrl = (search, time) => {
+    const searchUriSegment = `&q=${search}`;
+    const timeUriSegment = time ? `&created_at[from]=${moment().subtract(Number(time), 'days').format('YYYY-MM-DD%2012:00:00')}` : '';
+
+    return `${SONGS_URL}${timeUriSegment}${searchUriSegment}`;
+};
+
+const playlistUrl = (playlist) => {
+    const [type] = playlist.split('|');
+
+    if (SESSION_STREAM_PLAYLIST) {
+        return SESSION_STREAM_URL;
+    }
+
+    switch (type) {
+        case GENRE_PLAYLIST_TYPE:
+            return genrePlaylistUrl(playlist);
+        case SEARCH_PLAYLIST_TYPE:
+            return searchPlaylistUrl(playlist);
+        default:
+            return '';
+    }
+};
+
+const playlistNextUrl = (
+        playlist,
+        playlists,
+        oauthToken,
+    ) => (playlist in playlists && playlists[playlist].nextUrl ?
+        `${playlists[playlist].nextUrl}${oauthToken ? `&oauth_token=${oauthToken}` : ''}`
+    : null
+);
+
+const playlistSongs = (playlist, playlists, entities) => (playlist in playlists
+    ? denormalize(playlists[playlist].items, [songSchema], entities)
+    : []
 );
 
 export const playlistData = (
@@ -78,3 +130,5 @@ export const playlistData = (
         songs: playlistSongs(playlist, playlists, entities),
     };
 };
+
+export default playlistUrl;
